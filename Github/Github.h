29ed14e89d15065b12fdcf0b7527f0de104a8e3d
yutuno <yuto_uno51@yahoo.co.jp>
2019-04-18 12:33:31 +0900

    Github workspaceを作成

---
+++ Github/Github.h
@@ -0,0 +1,19 @@
+//
+//  Github.h
+//  Github
+//
+//  Created by yuto on 2019/04/18.
+//  Copyright © 2019 yuto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Github.
+FOUNDATION_EXPORT double GithubVersionNumber;
+
+//! Project version string for Github.
+FOUNDATION_EXPORT const unsigned char GithubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Github/PublicHeader.h>
+
+
